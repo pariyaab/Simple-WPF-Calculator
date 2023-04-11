1962ee3c52db218ed21171490f5a7dc408d25ad6
paria.abadeh <paria.abadeh@partsoftware.com>
2023-04-11 20:04:05 +0330

    Power action is added

---
+++ Calculator/Models/CalculatorModel.cs
@@ -0,0 +1,177 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using org.mariuszgromada.math.mxparser;
+using System;
+namespace Calculator.Models
+{
+    public interface IOperations
+    {
+        void Insert(string digit);
+        void InsertOperation(Operations operation);
+    }
+
+    public enum Operations
+    {
+        Clear,
+        Equal,
+        Backspace,
+        Bracket,
+    }
+
+    public class CalculatorModel : IOperations
+    {
+        public string Expression { get; private set; } = string.Empty;
+        public string Result { get; private set; } = string.Empty;
+
+
+        private const string OpenBracketSign = "(";
+        private const string EncloseBracketSign = ")";
+
+        private readonly Stack<string> _encloseBracketStack;
+
+        public CalculatorModel()
+        {
+            _encloseBracketStack = new Stack<string>();
+        }
+
+        private void Clear()
+        {
+            Expression = string.Empty;
+            Result = string.Empty;
+        }
+
+        private void Equal()
+        {
+            Expression = Result;
+        }
+
+        private void Backspace()
+        {
+            if (string.IsNullOrEmpty(Expression)) return;
+
+            if (Regex.IsMatch(Expression, $@"\{EncloseBracketSign}$"))
+                _encloseBracketStack.Push(EncloseBracketSign);
+            else if (Regex.IsMatch(Expression, $@"\{OpenBracketSign}$"))
+                _encloseBracketStack.Pop();
+
+
+            Expression = Expression.Remove(Expression.Length - 1, 1);
+            Result = CalculateExpression();
+        }
+
+        private void AddBracket()
+        {
+            if (!_encloseBracketStack.Any())
+            {
+                Expression += Regex.IsMatch(Expression.Last().ToString(), @"\d") ? $"*{OpenBracketSign}" : OpenBracketSign;
+                _encloseBracketStack.Push(EncloseBracketSign);
+                Result = string.Empty;
+            }
+            else
+            {
+                Expression += _encloseBracketStack.Pop();
+                Result = CalculateExpression();
+            }
+        }
+
+        public void Insert(string element)
+        {
+            if (Regex.IsMatch(element, @"[+\-*/%,^]"))
+                Expression += string.IsNullOrEmpty(Expression) || Regex.IsMatch(Expression, @"[+\-*/%]$") ? string.Empty : element;
+            else
+                Expression += element;
+
+            Result = CalculateExpression();
+        }
+
+        public void InsertOperation(Operations operation)
+        {
+            switch (operation)
+            {
+                case Operations.Backspace:
+                    Backspace();
+                    break;
+                case Operations.Clear:
+                    Clear();
+                    break;
+                case Operations.Bracket:
+                    AddBracket();
+                    break;
+                case Operations.Equal:
+                    Equal();
+                    break;
+            }
+        }
+
+        string CalculateExpression()
+        {
+            if (string.IsNullOrEmpty(Expression) || !Regex.IsMatch(Expression.Last().ToString(), @"(\d|\))") || _encloseBracketStack.Any())
+                return string.Empty;
+
+            Expression expression = new Expression(Expression);
+
+            #region MyRegion
+
+            //var listOfDigits = Regex.Split(Expression, @"\D").Select(Convert.ToDouble).ToList();
+            //var listOfOperations = Regex.Split(Expression, @"((\d*\,\d+)|\d+)").Select(sign =>
+            //{
+            //    switch (sign)
+            //    {
+            //        case "+":
+            //            return Operations.Add;
+            //        case "-":
+            //            return Operations.Substruct;
+            //        case "*":
+            //            return Operations.Multiply;
+            //        default:
+            //            return Operations.Divide;
+            //    }
+            //}).ToList();
+
+
+            //if (listOfDigits.Count > listOfOperations.Count)
+            //{
+            //    CurrentOperation = Operations.Add;
+            //    var result = 0.0;
+            //    for (int index = 0; index < listOfDigits.Count; index++)
+            //    {
+            //        var digit = listOfDigits[index];
+            //        switch (CurrentOperation)
+            //        {
+            //            case Operations.Add:
+            //                result += digit;
+            //                break;
+            //            case Operations.Substruct:
+            //                result -= digit;
+            //                break;
+            //            case Operations.Multiply:
+            //                result *= digit;
+            //                break;
+            //            case Operations.Divide:
+            //                result /= digit;
+            //                break;
+            //            default:
+            //                throw new ArgumentOutOfRangeException();
+            //        }
+
+            //        if (index > listOfOperations.Count)
+            //        {
+
+            //        }
+            //        else
+            //        {
+            //            CurrentOperation = listOfOperations[index];
+            //        }
+            //    }
+            //    return result.ToString(CultureInfo.InvariantCulture);
+            //}
+            //return string.Empty;
+
+
+            #endregion
+            return expression.calculate().ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Calculator/ViewModels/CalculatorViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Calculator.Commands;
+using Calculator.Models;
+
+namespace Calculator.ViewModels
+{
+
+    public class CalculatorViewModel : ViewModelBase, IOperations
+    {
+        private readonly CalculatorModel _model;
+
+        public string Expression => _model.Expression;
+        public string Result => _model.Result;
+
+        public ICommand InsertCommand => new DelegatingCommand(o => Insert((string)o));
+        public void Insert(string digit)
+        {
+            _model.Insert(digit);
+            OnPropertyChanged(nameof(Expression));
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public ICommand InsertOperationCommand => new DelegatingCommand(o => InsertOperation((Operations)o));
+        public void InsertOperation(Operations operation)
+        {
+            _model.InsertOperation(operation);
+            OnPropertyChanged(nameof(Expression));
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public CalculatorViewModel()
+        {
+            _model = new CalculatorModel();
+        }
+    }
+}
